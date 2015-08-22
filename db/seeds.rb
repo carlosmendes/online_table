5295b24cf356ad030b0c3e0ae8f7723f7a118ed0
Carlos Mendes <carlosmendes@Carloss-MBP.home>
2015-08-22 18:43:56 +0100

    users seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ categories = Category.create([{ name: 'Entrée', order: 1, active: true},
 products = Product.create([{ name: 'Beer 50cl', short_name: 'Beer 50cl', price: 1, description: 'Sagres', active: true},
                            { name: 'Beer 75cl', short_name: 'Beer 75cl', price: 1.2, description: 'Sagres', active: true},
                            { name: 'Bitoque', short_name: 'Bitoque', price: 5, description: 'Com ovo', active: true}])
+                           
+users = User.create([{name: 'Manager', email: 'manager@demo.com', password: '123456', confirmed: true, manager: true},
+                     {name: 'Waiter', email: 'waiter@demo.com', password: '123456', confirmed: true, waiter: true},
+                     {name: 'Client', email: 'client@demo.com', password: '123456', confirmed: true, manager: false}])  
